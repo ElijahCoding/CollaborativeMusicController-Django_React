3a11f916846bbc83f4e6eafe2504d2e761516940
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-09 22:22:03 +0300

    room view buit

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
 class RoomView(generics.ListAPIView):
-    pass
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
