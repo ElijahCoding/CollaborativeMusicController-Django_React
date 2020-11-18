ead266d82873cf105c8e2be6b83705f08d172786
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-18 22:38:21 +0300

    creating room endpoint

--- api/views.py
+++ api/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer
+
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        pass
